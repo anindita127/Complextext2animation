8e9f7c434d2b4178d6107faf3f8f5b6983ef42c6
Anindita Ghosh <anindita127@gmail.com>
2022-02-27 21:26:23 +0100

    Update eval_AVE.py

--- src/eval_AVE.py
+++ src/eval_AVE.py
@@ -94,7 +94,7 @@ def sample(args, exp_num, data=None):
 
     # hardcoded for sampling
     batch_size = args.batch_size
-    time = args.time
+    time = 64
     chunks = args.chunks
     offset = args.offset
     # mask for delta
